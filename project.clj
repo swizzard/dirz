3a3536a43072643eb24227172c226bb4cb61b60b
Sam Raker <sam.raker@gmail.com>
2015-04-19 19:58:04 -0400

    add JSON-producing stuff

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                  [instaparse "1.3.6"]]
+                  [instaparse "1.3.6"]
+                  [cheshire "5.4.0"]]
   :main ^:skip-aot dirz.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/dirz/parsing.clj
+++ src/dirz/parsing.clj
@@ -1,5 +1,8 @@
 (ns dirz.parsing
-  (:require [instaparse.core :as insta]))
+  (:require [instaparse.core :as insta]
+            [cheshire.core :refer [generate-string
+                                   generate-stream]]
+            [clojure.java.io :as io]))
 
 (def ini-parser
   "Instaparse parser for .ini files"
@@ -12,10 +15,14 @@
                  <K> = #'\\w+'
                  <V> = #'\\w+'"))
 
+(defn- merge-sections [ms]
+  (reduce merge (map (fn [m] {(keyword (:Section m))
+                        (dissoc m :Section)})
+                      ms)))
 (defn to-map
   "Convert a parsed ini to a clojure map"
   [parsed-ini]
-  (map (partial apply hash-map) parsed-init))
+  (merge-sections (map (partial apply hash-map) parsed-ini)))
 
 (defn parse-ini
   "Convert an ini to a map"
@@ -26,3 +33,11 @@
   "Load an ini file as a map"
   [^String ini-file]
   (-> (slurp ini-file) parse-ini))
+
+(defn ini-to-json
+  "Write an ini file to JSON"
+  ([^String ini-text]
+   (generate-string (parse-ini ini-text) {:pretty true}))
+  ([^String ini-text ^String dest-file]
+   (generate-stream (parse-ini ini-text)
+                    (io/writer dest-file) {:pretty true})))
